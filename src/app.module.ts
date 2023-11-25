93ff1bf85392e31e6676d9b26798378fbe106383
Igor Kachynskyy <igor.kachynskyy@gmail.com>
2023-11-25 14:19:41 +0200

    feature: init project

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/../src/common/envs`);
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Booking Friends API')
+    .setDescription('The Booking Friends API is a robust and user-friendly interface designed to streamline and enhance the experience of booking travel and accommodation. It enables seamless integration with various booking systems, allowing users to search, compare, and reserve hotels, flights, and other travel services with ease. This API is particularly beneficial for travel agencies, hospitality businesses, and individual travelers seeking a convenient and efficient way to manage their travel plans.')
+    .setVersion('1.0')
+    .addTag('Booking')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('booking-api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
